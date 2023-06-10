063d80d1e8fb87b9c04bac926d0607a77c7a9845
kussakaa <kussakaa@proton.me>
2023-06-10 22:31:27 +0200

    Добавлена библиотека freetype

--- build.zig
+++ build.zig
@@ -10,6 +10,7 @@ pub fn build(b: *Builder) void {
     exe.linkSystemLibrary("c");
     exe.linkLibC();
     exe.linkSystemLibrary("glfw3");
+    exe.linkSystemLibrary("freetype");
     exe.addIncludePath("deps/glad/include");
     exe.addCSourceFile("deps/glad/src/glad.c", &[_][]const u8{"-std=c99"});
     exe.setTarget(target);
