7a2c89ff10ae7845f4378ea3f537fb767758b2d1
kussakaa <kussakaa@proton.me>
2023-09-02 02:11:33 +0200

    Очищен репозиторий

--- src/c.zig
+++
@@ -1,7 +0,0 @@
-pub usingnamespace @cImport({
-    @cInclude("glad/glad.h");
-    @cInclude("SDL2/SDL.h");
-    @cInclude("ft2build.h");
-    @cInclude("freetype/freetype.h");
-    @cInclude("FastNoiseLite.h");
-});
