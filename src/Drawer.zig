2e5d64006812f22f7a3b14bc7de182d72ced7857
kussakaa <kussakaa@proton.me>
2024-07-24 20:21:59 +0200

    added debug tree for light

--- src/Drawer.zig
+++ src/Drawer.zig
@@ -5,6 +5,14 @@ world: struct {
     ctx: *const World,
     camera: Gfx.Camera,
     program: Gfx.Program,
+
+    light: struct {
+        color: zm.Vec,
+        dir: zm.Vec,
+        ambient: f32,
+        diffuse: f32,
+        specular: f32,
+    },
 },
 
 gui: struct {
@@ -41,6 +49,13 @@ pub fn init(allocator: Allocator, config: Config) !Self {
                     "light.specular",
                 },
             }),
+            .light = .{
+                .color = .{ 1.0, 1.0, 1.0, 1.0 },
+                .dir = .{ 0.0, 0.0, 1.0, 1.0 },
+                .ambient = 0.3,
+                .diffuse = 0.2,
+                .specular = 3,
+            },
         },
         .gui = .{
             .ctx = config.gui.ctx,
@@ -63,6 +78,11 @@ pub fn draw(self: *Self) bool {
     {
         const world = self.world;
         world.program.use();
+        world.program.uniforms[0].set(world.light.color); // light.color
+        world.program.uniforms[1].set(world.light.dir); // light.dir
+        world.program.uniforms[2].set(world.light.ambient); // light.ambient
+        world.program.uniforms[3].set(world.light.diffuse); // light.diffuse
+        world.program.uniforms[4].set(world.light.specular); // light.specular
     }
 
     // GUi
@@ -72,15 +92,27 @@ pub fn draw(self: *Self) bool {
     const fb_size = self.gfx.window.handle.getFramebufferSize();
     imgui.backend.newFrame(@intCast(fb_size[0]), @intCast(fb_size[1]));
 
-    if (imgui.begin("Camera", .{})) {
-        _ = imgui.sliderFloat4("pos", .{ .v = &self.world.camera.pos, .min = 0.0, .max = 32.0 });
-        _ = imgui.sliderFloat4("rot", .{ .v = &self.world.camera.rot, .min = 0.0, .max = std.math.pi * 2 });
-        _ = imgui.sliderFloat("scale", .{ .v = &self.world.camera.scale, .min = 0.1, .max = 32.0 });
-        _ = imgui.sliderFloat("ratio", .{ .v = &self.world.camera.ratio, .min = 0.5, .max = 2.0 });
-    }
-    imgui.end();
-    imgui.backend.draw();
+    if (imgui.begin("debug", .{})) {
+        if (imgui.treeNode("camera")) {
+            _ = imgui.sliderFloat4("pos", .{ .v = &self.world.camera.pos, .min = 0.0, .max = 32.0 });
+            _ = imgui.sliderFloat4("rot", .{ .v = &self.world.camera.rot, .min = 0.0, .max = std.math.pi * 2 });
+            _ = imgui.sliderFloat("scale", .{ .v = &self.world.camera.scale, .min = 0.1, .max = 32.0 });
+            _ = imgui.sliderFloat("ratio", .{ .v = &self.world.camera.ratio, .min = 0.5, .max = 2.0 });
+            imgui.treePop();
+        }
 
+        if (imgui.treeNode("light")) {
+            _ = imgui.sliderFloat4("color", .{ .v = &self.world.light.color, .min = 0.0, .max = 1.0 });
+            _ = imgui.sliderFloat4("direction", .{ .v = &self.world.light.dir, .min = -1.0, .max = 1.0 });
+            _ = imgui.sliderFloat("ambient", .{ .v = &self.world.light.ambient, .min = 0.0, .max = 1.0 });
+            _ = imgui.sliderFloat("diffuse", .{ .v = &self.world.light.diffuse, .min = 0.0, .max = 1.0 });
+            _ = imgui.sliderFloat("specular", .{ .v = &self.world.light.specular, .min = 0.0, .max = 10.0 });
+            imgui.treePop();
+        }
+        imgui.end();
+    }
+
+    imgui.backend.draw();
     self.gfx.swapBuffers();
     return true;
 }
@@ -92,6 +124,7 @@ const Allocator = std.mem.Allocator;
 const World = @import("World.zig");
 const Gui = @import("Gui.zig");
 
+const zm = @import("zmath");
 const gl = @import("zopengl").bindings;
 const imgui = @import("zgui");
 const std = @import("std");
